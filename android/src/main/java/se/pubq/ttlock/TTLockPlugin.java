944f6c92c6642a3c70999653999e682d7b8000e8
Henrik Walldén <hwallden@gmail.com>
2020-05-21 17:02:07 +0200

    updates

--- android/src/main/java/se/pubq/ttlock/TTLockPlugin.java
+++ android/src/main/java/se/pubq/ttlock/TTLockPlugin.java
@@ -9,7 +9,10 @@ import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.ttlock.bl.sdk.api.ExtendedBluetoothDevice;
 import com.ttlock.bl.sdk.api.TTLockClient;
+import com.ttlock.bl.sdk.callback.ControlLockCallback;
+import com.ttlock.bl.sdk.callback.InitLockCallback;
 import com.ttlock.bl.sdk.callback.ScanLockCallback;
+import com.ttlock.bl.sdk.constant.ControlAction;
 import com.ttlock.bl.sdk.entity.LockError;
 
 @NativePlugin(
@@ -25,21 +28,50 @@ public class TTLockPlugin extends Plugin {
 
     @PluginMethod()
     public void echo(final PluginCall call) {
-        TTLockClient.getDefault().prepareBTService(getContext());
-//https://gitee.com/sciener/Android-TTLock-SDK-V3-Demo
-        TTLockClient.getDefault().startScanLock(new ScanLockCallback() {
+        final JSObject returnData = new JSObject();
+        final TTLockClient lockClient = TTLockClient.getDefault();
+        lockClient.prepareBTService(getContext());
+        lockClient.startScanLock(new ScanLockCallback() {
             @Override
-            public void onScanLockSuccess(ExtendedBluetoothDevice device) {
-                JSObject returnObject = new JSObject();
-                returnObject.put("value", device.toString());
-                call.success(returnObject);
-            }
+            public void onScanLockSuccess(final ExtendedBluetoothDevice device) {
 
+                lockClient.initLock(device, new InitLockCallback() {
+                    @Override
+                    public void onInitLockSuccess(String lockData, int specialValue) {
+                        returnData.put("lockData",lockData);
+                        returnData.put("specialValue",specialValue);
+                        call.resolve(returnData);
+                        lockClient.stopBTService();
+////                        returnObject.put("lockData", lockData);
+////                        returnObject.put("specialValue",specialValue);
+////                        lockClient.controlLock(ControlAction.UNLOCK, lockData, device.getAddress(), new ControlLockCallback() {
+////                            @Override
+////                            public void onControlLockSuccess(int lockAction, int battery, int uniqueId) {
+////                                returnObject.put("lockAction",lockAction);
+////                                returnObject.put("battery",battery);
+////                                returnObject.put("uniqueId",uniqueId);
+////                                //lockClient.stopBTService();
+////                                //call.resolve(returnObject);
+////                            }
+////                            @Override
+////                            public void onFail(LockError error) {
+////
+////                            }
+////                        });
+                    }
+//
+                    @Override
+                    public void onFail(LockError error) {
+
+                    }
+                });
+            }
+//
             @Override
             public void onFail(LockError error) {
-                call.reject("Failed lock scan");
             }
         });
+        }
 
     }
-}
+
