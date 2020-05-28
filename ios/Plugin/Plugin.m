64745a2712ec1f1f4bccae38c4ad3f4f490b7f99
Henrik Walldén <henrikwallden@Henriks-Mac-mini.local>
2020-05-29 00:25:47 +0200

    changes

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -1,8 +1,8 @@
 #import <Foundation/Foundation.h>
 #import <Capacitor/Capacitor.h>
-#import <TTLock/TTLock.h>
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(TTLockPlugin, "TTLockPlugin",
            CAP_PLUGIN_METHOD(unlock, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(setupBluetooth, CAPPluginReturnPromise);
 )
--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -1,32 +1,29 @@
 import Foundation
 import Capacitor
-
+import TTLock
 /**
  * Please read the Capacitor iOS Plugin Development Guide
  * here: https://capacitor.ionicframework.com/docs/plugins/ios
  */
 @objc(TTLockPlugin)
 public class TTLockPlugin: CAPPlugin {
-    
+    @objc func setupBluetooth(_ call: CAPPluginCall){
+        TTLock.setupBluetooth({ state in
+            print(String(format: "###### TTLock is working, bluetooth state: %ld ######", Int(state)))
+            call.resolve("Bluetooth working!");    
+        })
+    }
+
     @objc func unlock(_ call: CAPPluginCall) {
         let value = call.getString("value") ?? "";
         let lockData = call.getString("lockData");
         let lockMac = call.getString("lockMac");
-        [TTLock setupBluetooth:^(TTBluetoothState state) {
-            NSLog(@"##############  TTLock is working, bluetooth state: %ld  ##############",(long)state);
-            
-            [TTLock controlLockWithControlAction:TTControlActionUnlock lockData:lockData success:^(long long lockTime, NSInteger electricQuantity, long long uniqueId) {
-            NSLog(@"##############  Unlock successed power: %ld  ##############",(long)electricQuantity);
-             call.success([
-            "lockTime": lockTime,
-            "uniqueId": uniqueId
-                ])
-            } failure:^(TTError errorCode, NSString *errorMsg) {
-            NSLog(@"##############  Unlock failed errorMsg: %@  ##############",errorMsg);
-            call.reject(errorMsg);
-            }];
-        
-        }];
-       
+        TTlock.controlLock(withControlAction: TTControlActionUnlock, lockData:lockData,success:{lockTime,electricQuantity,uniqueId in
+            print(String(format: "###### Unlock success power %ld #####",electricQuantity))
+            call.resolve([uniqueId:uniqueId,electricQuantity:electricQuantity]);
+        }, failure: {
+            errorCode,errorMsg in
+            print("######## Unlock failed errorMsg: \(errorMsg ?? "") ######");
+        });
     }
 }
