42b9f0c9788efec2d263de25a77959edc003dac9
Henrik Walldén <henrikwallden@Henriks-Mac-mini.local>
2020-05-29 03:04:41 +0200

    testing

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -9,21 +9,21 @@ import Capacitor
 public class TTLockPlugin: CAPPlugin {
     @objc func setupBluetooth(_ call: CAPPluginCall){
         TTLock.setupBluetooth({ state in
-            print(String(format: "###### TTLock is working, bluetooth state: %ld ######", Int(state)))
-            call.resolve("Bluetooth working!");    
+            print(String(format: "###### TTLock is working, bluetooth state: %ld ######", state.rawValue))
+            call.resolve(["msg":"Bluetooth working!"]);
         })
     }
 
     @objc func unlock(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? "";
         let lockData = call.getString("lockData");
-        let lockMac = call.getString("lockMac");
-        TTlock.controlLock(withControlAction: TTControlActionUnlock, lockData:lockData,success:{lockTime,electricQuantity,uniqueId in
+        //let lockMac = call.getString("lockMac");
+        TTLock.controlLock(with: TTControlAction.actionUnlock, lockData:lockData,success:{lockTime,electricQuantity,uniqueId in
             print(String(format: "###### Unlock success power %ld #####",electricQuantity))
-            call.resolve([uniqueId:uniqueId,electricQuantity:electricQuantity]);
+            call.resolve(["uniqueId":uniqueId,"electricQuantity":electricQuantity]);
         }, failure: {
             errorCode,errorMsg in
             print("######## Unlock failed errorMsg: \(errorMsg ?? "") ######");
+            call.reject(errorMsg ?? "");
         });
     }
 }
